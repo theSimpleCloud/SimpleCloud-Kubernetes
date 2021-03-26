b06c4fc169976e34216a798ce94c2b5a668c4560
Wetterbericht <baier.frederick@gmail.com>
2021-03-26 18:57:31 +0100

    Configure subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,25 @@ plugins {
     kotlin("jvm") version "1.4.31"
 }
 
-group = "eu.thesimplecloud"
-version = "3.0.0-SNAPSHOT"
+allprojects {
+    group = "eu.thesimplecloud"
+    version = "3.0.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+    repositories {
+        mavenCentral()
+
+    }
 }
 
-dependencies {
-    implementation(kotlin("stdlib"))
-    testCompile("junit", "junit", "4.12")
+subprojects {
+    apply(java)
+
+    dependencies {
+        implementation(kotlin("stdlib"))
+        testCompile("junit", "junit", "4.12")
+    }
 }
+
+
+
+
