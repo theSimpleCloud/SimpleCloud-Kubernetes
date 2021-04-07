f443ea9d1d206a92225726e2ea5b6456a94f96bf
Philipp Eistrach <p.eistrach@gmail.com>
2021-04-07 21:23:04 +0200

    Cleanup code

--- application-loader/src/main/kotlin/eu/thesimplecloud/application/loader/AbstractApplicationLoader.kt
+++ application-loader/src/main/kotlin/eu/thesimplecloud/application/loader/AbstractApplicationLoader.kt
@@ -1,13 +1,8 @@
 package eu.thesimplecloud.application.loader
 
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.ObjectMapper
 import eu.thesimplecloud.application.ILoadedApplication
-import eu.thesimplecloud.application.data.DefaultApplicationData
 import eu.thesimplecloud.application.data.IApplicationData
 import java.io.File
-import java.util.jar.JarEntry
-import java.util.jar.JarFile
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,46 +10,19 @@ import java.util.jar.JarFile
  * Date: 26.03.2021
  * Time: 21:17
  */
-abstract class AbstractApplicationLoader<D : IApplicationData> {
+abstract class AbstractApplicationLoader<T : ILoadedApplication<*>>(
+    private val jarFileLoader: AbstractJarFileLoader<*>
+) {
 
-    private val objectMapper = ObjectMapper()
-
-    fun loadAllApplications(directory: File): List<ILoadedApplication<*, *>> {
+    fun loadAllApplications(directory: File): List<T> {
         require(directory.isDirectory)
         return directory.listFiles()?.map {
-            val applicationData = loadJsonFileInJar(it)
+            val applicationData = jarFileLoader.loadJsonFileInJar(it)
             loadApplication(it, applicationData)
         } ?: emptyList()
     }
 
-
-    fun loadJsonFileInJar(file: File, path: String): D {
-        require(file.exists()) { "Specified file to load $path from does not exist: ${file.path}" }
-        try {
-            val jar = JarFile(file)
-            val entry: JarEntry = jar.getJarEntry(path) ?: throw IllegalStateException("test")
-            val fileStream = jar.getInputStream(entry)
-
-            val jsonNode = objectMapper.readTree(fileStream)
-            jar.close()
-
-            val defaultApplicationData = constructDefaultApplicationData(jsonNode)
-
-            return constructApplicationData(defaultApplicationData, jsonNode)
-        } catch (ex: Exception) {
-            throw IllegalStateException("test")
-        }
-    }
-
-    private fun constructDefaultApplicationData(jsonNode: JsonNode): DefaultApplicationData {
-        return DefaultApplicationData.fromJsonNode(jsonNode)
-    }
-
-    abstract fun loadApplication(file: File, applicationData: D): ILoadedApplication<*, *>
-
-    abstract fun loadJsonFileInJar(file: File): D
-
-    abstract fun constructApplicationData(defaultApplicationData: DefaultApplicationData, jsonNode: JsonNode): D
+    abstract fun loadApplication(file: File, applicationData: IApplicationData): T
 
     abstract fun createModuleClassLoader(file: File): ClassLoader
 
