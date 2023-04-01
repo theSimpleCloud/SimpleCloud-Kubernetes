ec7d21d2d6841fea94cc4f311a7667accb1befb3
Frederick Baier <baier.frederick@gmail.com>
2023-04-01 17:35:01 +0200

    Update minestom dependency

--- plugin-parent/minestom/build.gradle.kts
+++ plugin-parent/minestom/build.gradle.kts
@@ -1,7 +1,7 @@
 
 
 dependencies {
-    compileOnly("com.github.Minestom:Minestom:067227421f")
+    compileOnly("com.github.Minestom:Minestom:4f3c35ee87")
     api(project(":plugin-parent:plugin"))
     implementation("app.simplecloud:distribution-hazelcast:1.0.4-SNAPSHOT")
 }
